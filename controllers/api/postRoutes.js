6216f4b47d5e5b8c169759ebbc0d4826cb45c48c
aq032 <anthonyq032@gmail.com>
2022-09-07 21:24:56 -0700

    Posts can now be created by POST request. GET request for all posts as well

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,60 @@
+const router = require("express").Router();
+const { Post, User, Comment } = require("../../models");
+const sequelize = require("../../config/connection");
+
+// All requests are made to /api/posts/...
+
+// Get all posts
+router.get("/", (req, res) => {
+  Post.findAll({
+    include: [
+      {
+        model: User,
+        attributes: ["name"]
+      }
+    ]
+  })
+  .then(postData => res.json(postData))
+  .catch(err => {
+    res.status(500).json(err);
+  })
+})
+
+// Get Post by ID
+router.get("/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: User,
+        attributes: ["name"]
+      }
+    ]
+  })
+  .then(postData => {
+    if(!postData) {
+      res.status(404).json({ message: "No post found with this id!"})
+    }
+    res.json(postData)
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  })
+})
+
+// Create new post
+router.post("/", (req, res) => {
+  Post.create({
+    title: req.body.title,
+    description: req.body.description,
+    user_id: req.body.user_id
+  })
+  .then(postData => res.json(postData))
+  .catch(err => {
+    res.status(500).json(err);
+  })
+})
+
+module.exports = router;
