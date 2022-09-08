15ed98dc572fdf0c7061055c48a71183f668c700
aq032 <anthonyq032@gmail.com>
2022-09-07 21:58:57 -0700

    Added GET request to get user information by ID

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,12 +1,43 @@
 // Temporary code
 const router = require("express").Router();
-const { User } = require("../../models");
+const { User, Post } = require("../../models");
 
 // All requests are made to /api/users/...
 
 router.get("/", async (req, res) => {
   try {
     const userData = await User.findAll({ attributes: { exclude: ["password"] } });
+
+    res.status(200).json(userData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
+router.get("/:id", async (req, res) => {
+  try {
+    const userData = await User.findOne({ 
+      attributes: {
+        exclude: ["password"]
+      },
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: Post,
+          attributes: ["id", "title", "description", "date_created"]
+        },
+        // {
+        //   model: Comment,
+        //   attributes: ["id", "post_id", "description", "date_created"],
+        //   include: {
+        //     model: Post,
+        //     attributes: ["title"]
+        //   }
+        // }
+      ]
+    });
 
     res.status(200).json(userData)
   } catch (err) {
