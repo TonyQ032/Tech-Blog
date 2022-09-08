f811e2827226ee089c338ce2219e7db510b3e749
aq032 <anthonyq032@gmail.com>
2022-09-07 21:46:43 -0700

    Converted routes to try/catch statements

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -5,74 +5,82 @@ const sequelize = require("../../config/connection");
 // All requests are made to /api/posts/...
 
 // Get all posts
-router.get("/", (req, res) => {
-  Post.findAll({
-    include: [
-      {
-        model: User,
-        attributes: ["name"]
-      }
-    ]
-  })
-  .then(postData => res.json(postData))
-  .catch(err => {
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name"]
+        }
+      ]
+    });
+
+    res.json(postData)
+  } catch (err) {
     res.status(500).json(err);
-  })
+  }
 })
 
 // Get Post by ID
-router.get("/:id", (req, res) => {
-  Post.findOne({
-    where: {
-      id: req.params.id
-    },
-    include: [
-      {
-        model: User,
-        attributes: ["name"]
-      }
-    ]
-  })
-  .then(postData => {
-    if(!postData) {
-      res.status(404).json({ message: "No post found with this id!"})
+router.get("/:id", async (req, res) => {
+  try {
+    const postData = await Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: User,
+          attributes: ["name"]
+        }
+      ]
+    })
+
+    if (!postData) {
+      res.status(404).json({ message: "No post found with this id!" })
     }
     res.json(postData)
-  })
-  .catch(err => {
+
+  } catch {
     res.status(500).json(err);
-  })
+  }
+
 })
 
 // Create new post
-router.post("/", (req, res) => {
-  Post.create({
-    title: req.body.title,
-    description: req.body.description,
-    user_id: req.body.user_id
-  })
-  .then(postData => res.json(postData))
-  .catch(err => {
+router.post("/", async (req, res) => {
+  try {
+    const postData = await Post.create({
+      title: req.body.title,
+      description: req.body.description,
+      user_id: req.body.user_id
+    })
+
+    res.json(postData)
+  } catch (err) {
     res.status(500).json(err);
-  })
+  }
 })
 
-router.put("/:id", (req, res) => {
-  Post.update(
-    {
-      title: req.body.title,
-      description: req.body.description
-    },
-    {
-      where: {
-        id: req.params.id
+router.put("/:id", async (req, res) => {
+  try {
+    const postData = await Post.update(
+      {
+        title: req.body.title,
+        description: req.body.description
+      },
+      {
+        where: {
+          id: req.params.id
+        }
       }
-    }
-  )
-  .then(postData => res.json({"Message": "Post updated successfully!", "Post": postData}))
-  .catch(err => {
-    res.status(500).json(err);
-  })
+    )
+
+    res.json({ "Message": "Post updated successfully!"})
+  } catch (err) {
+    res.status(500).json(err)
+  }
 })
 
 module.exports = router;
