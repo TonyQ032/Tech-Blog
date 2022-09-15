843e7cbd6bed96bc85997ac80f9dc9e28aaa41b2
aq032 <anthonyq032@gmail.com>
2022-09-15 11:27:43 -0700

    Added universal redirect and postudate page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -45,6 +45,7 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Get page for complete, individual post and its comments
 router.get("/post/:id", async (req, res) => {
   try {
 
@@ -81,6 +82,28 @@ router.get("/post/:id", async (req, res) => {
     } else {
       res.redirect("/login");
     }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Redirects user to the page where they can update their posts
+router.get("/post/update/:id", auth, async (req, res) => {
+  try {
+    const postData = await Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: ["id", "title", "description"]
+    })
+
+    const post = postData.get({ plain: true });
+
+    res.status(200).render("updatepost", {
+      post,
+      logged_in: req.session.logged_in,
+      user_id: req.session.user_id
+    })
   } catch (err) {
     res.status(500).json(err);
   }
@@ -140,6 +163,48 @@ router.get("/dashboard", auth, async (req, res) => {
       res.json(user)
     }
 
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Universal route if user tries to input an invalid URL
+router.get("*", async (req, res) => {
+  try {
+
+    // Retrieve all posts from database
+    const postData = await Post.findAll({
+      attributes: ["id", "description", "date_created", "title"],
+      order: sequelize.literal('id DESC'),
+      include: [
+        {
+          model: Comment,
+          attributes: ["id", "description", "post_id", "user_id", "date_created"],
+          include: {
+            model: User,
+            attributes: ["name"]
+          }
+        },
+        {
+          model: User,
+          attributes: ["name"]
+        }
+      ]
+    });
+
+    const posts = postData.map(post => post.get({ plain: true }));
+
+    if (req.session.logged_in) {
+      res.render("homepage", {
+        posts,
+        logged_in: req.session.logged_in,
+        user_id: req.session.user_id
+      })
+    } else {
+      res.render("homepage", {
+        posts
+      });
+    }
   } catch (err) {
     res.status(500).json(err);
   }
